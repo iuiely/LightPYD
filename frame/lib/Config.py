53de3e5183a60ec790ac9a37ddb62e2db9a8cfc5
iuiely <iuie@163.com>
2019-11-04 23:14:39 +0800

    change Config.py

--- frame/lib/Config.py
+++ frame/lib/Config.py
@@ -1,4 +1,4 @@
-import os
+import os,sys,pdb
 from sys import version_info
 if version_info.major == 2 and version_info.minor ==7:
     import ConfigParser
@@ -7,33 +7,65 @@ elif version_info.major == 3 :
     import configparser
     configparser = configparser.ConfigParser()
 
-
 class Config(object):
-
-    cfg_dict = {}
-
     def __init__(self):
         self.cfg=None
-
     @classmethod
-    def DictMake(cls,_file):
-        if os.access(_file, os.R_OK) :
-            conf =_file
-
+    def DictMake(cls,_file,path=None):
+        cls.cfg_dict ={}
+        if path is None:
+            if os.access(_file, os.R_OK) :
+                conf =_file
+        else:
+            if os.access(path+_file,os.R_OK):
+                conf = path+'/'+_file
         cls.cfg = configparser
+        for a in cls.cfg.sections() :
+            cls.cfg.remove_section(a)
         cls.cfg.read(conf)
-        #section = cfg.sections();
         for section in cls.cfg.sections():
             for key in cls.cfg.options(section) :
                 cls.cfg_dict[key] = cls.cfg.get(section,key)
-                
         return cls.cfg_dict
 
-'''
-if __name__ =='__main__':
-    _file = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))+'/frame/config.ini'
-    print(_file)
-    #config = Config()
-    res = Config.DictMake(_file)
-    print(res)
-'''
+    @classmethod
+    def get(cls,name,path=None):
+        cls.cfg_dict ={}
+        if name is None :
+            return False
+        _name = name.split('.')
+        num = len(_name)
+        if num == 1 :
+            _name = name.lower()
+            if path is None:
+                path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+                _file = path+'/config/'+_name+'.ini'
+            else:
+                _file = path+'/'+_name+'.ini'
+            if os.path.exists(_file) == False:
+                return False
+            cls.cfg = configparser
+            for a in cls.cfg.sections() :
+                cls.cfg.remove_section(a)
+            cls.cfg.read(_file)
+            for section in cls.cfg.sections():
+                for key in cls.cfg.options(section) :
+                    cls.cfg_dict[key] = cls.cfg.get(section,key)
+            return cls.cfg_dict
+        else:
+            _file_prefix = _name.pop(0).lower()
+            if path is None:
+                path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+                _file = path+'/config/'+_file_prefix+'.ini'
+            else:
+                _file = path+'/'+_file_prefix+'.ini'
+            if os.path.exists(_file) == False:
+                return False
+            cls.cfg = configparser
+            cls.cfg.read(_file)
+            section = _name.pop(0)
+            if len(_name) == 0:
+                return dict(cls.cfg.items(section))
+            else:
+                key=_name.pop(0)
+                return cls.cfg.get(section,key)
